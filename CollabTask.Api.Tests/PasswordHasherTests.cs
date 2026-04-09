9f440cabca826d203170727e681f98c9fca2fa71
sandroleibrecht <sandro.leibrecht@gmail.com>
2026-04-09 20:37:10 +0200

    Added PasswordHasher tests

---
+++ CollabTask.Api.Tests/PasswordHasherTests.cs
@@ -0,0 +1,49 @@
+﻿using CollabTask.Api.Shared.Helpers;
+
+namespace CollabTask.Api.Tests
+{
+	public abstract class PasswordHasherTests
+	{
+		protected abstract IPasswordHasher CreateHasher();
+
+		[Fact]
+		public void HashPassword_ShouldNotReturnPlaintext()
+		{
+			var hasher = CreateHasher();
+			var password = "SafeTestPassword123!";
+			var hash = hasher.Hash(password);
+
+			Assert.NotEqual(password, hash);
+		}
+
+		[Fact]
+		public void VerifyPasswordWithHash_ShouldReturnTrue()
+		{
+			var hasher = CreateHasher();
+			var password = "SafeTestPassword123!";
+			var hash = hasher.Hash(password);
+
+			var isVerified = hasher.Verify(password, hash);
+			
+			Assert.True(isVerified);
+		}
+
+		[Fact]
+		public void VerifyPassword_WithWrongPassword_ShouldReturnFalse()
+		{
+			var hasher = CreateHasher();
+			var password = "SafeTestPassword123!";
+			var hash = hasher.Hash(password);
+
+			var wrongPassword = "WrongPassword123!";
+			var isVerified = hasher.Verify(wrongPassword, hash);
+			
+			Assert.False(isVerified);
+		}
+	}
+
+	public class Argon2PasswordHasherTests : PasswordHasherTests
+	{
+		protected override IPasswordHasher CreateHasher() => new Argon2PasswordHasher();
+	}
+}
